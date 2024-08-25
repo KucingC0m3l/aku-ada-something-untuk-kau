b0722c39fee434839a826c597b9813afbe214929
R0ticCanai <roticanaitelurr@gmail.com>
2024-08-26 02:13:12 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const title = document.querySelector('.title')
+const text = 'I Have Something'.split('')
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== ' ') {
+    title.innerHTML += `<span>${text[index]}<span/>`
+  } else {
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+  }
+}
+
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 3; // Menghasilkan delay antara 0 saat hingga 3 saat
+  element.style.animationDelay = `${randomDelay}s`;
+});
